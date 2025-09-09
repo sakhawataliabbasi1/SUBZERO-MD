40adcabb79d5f70c0c5b8b13ae60d3dbb8f7272c
sakhawataliabbasi1 <sakhawataliabbasi831@gmail.com>
2025-09-09 09:23:38 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;LEU2yTKK#TOM-jnQAvsbzCwCZa3Pf_fQ2Boun1-kSK-euxubOjOs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923035702249", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
